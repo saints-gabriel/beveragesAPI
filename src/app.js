3e45fd541d6ae4029034090c42cf7c050f54be7c
saints-gabriel <gabrieldossantosmartins05@gmail.com>
2026-05-12 20:56:12 -0300

    /api landing page in html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import sequelize from './config/db.js';
 import wineRouter from './router/wine.route.js';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
-app.use('/wines', wineRouter); 
+app.use('/wines', wineRouter);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get('/api', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 sequelize.sync({ alter: true }).then( 
     ()=>{
